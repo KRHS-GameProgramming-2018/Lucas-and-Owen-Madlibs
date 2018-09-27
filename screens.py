34844901f377b03746429c75eb909beaa8915464
Lucas Carey <lucas.carey@kearsarge.org>
2018-09-27 08:25:34 -0400

    Day1

--- screens.py
+++ screens.py
@@ -2,21 +2,24 @@ def showSplash():
     output = ""
     output += "---------------------------------------\n"
     output += "|                                     |\n"
-    output += "|              MADLIBS                |\n"
-    output += "|                 by                  |\n"
-    output += "|              Some Students          |\n"
+    output += "|                MADLIBS              |\n"
+    output += "|                  by                 |\n"
+    output += "|            Lucas and Owen           |\n"
     output += "|                                     |\n"
     output += "---------------------------------------\n"
 
     
     return output
     
+    
+    
 def showMenu():
     output = ""
     output += "---------------------------------------\n"
     output += "|Main Menu:                           |\n"
-    output += "|   1) A Story by Spooner             |\n"
-    output += "|                                     |\n"
+    output += "|   1) 1st Story by Lucas and Owen    |\n"
+    output += "|   2) 2nd Story by Lucas             |\n"
+    output += "|   3) 3rd Story by Owen              |\n"
     output += "|   Q) Quit                           |\n"
     output += "|                                     |\n"
     output += "---------------------------------------\n"
