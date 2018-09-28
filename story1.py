d16bc20e742ee9414669316daf5dc4464ae7497f
Lucas Carey <lucas.carey@kearsarge.org>
2018-09-28 08:20:04 -0400

    Created first story

--- story1.py
+++ story1.py
@@ -2,14 +2,15 @@ from getInput import *
 
 def playMadlibs():
     friend1 = getWord("Enter a Name: ",)
-    numAnimals = getNumber("Enter a number: ", 7, 8)
-    animals1 = getWord("Enter a pluaral animal name: ")
+    numAnimals = getNumber("Enter a number: ", 7, 9)
+    animals1 = getWord("Enter a ball name: ")
+    object1 = getWord("enter an object: ")
     
     output = ""
     output += "One day I was walking with my friend, " + friend1
     output += ". Suddenly " + friend1
     output += " said that they saw " + numAnimals + " " + animals1
-    
+    output += " The friend took out his " + object1
     
     
     
