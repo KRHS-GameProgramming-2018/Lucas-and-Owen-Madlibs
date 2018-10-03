8c359e66dfe4ce838aaa110009c3ba6651bbaa32
Lucas Carey <lucas.carey@kearsarge.org>
2018-10-03 08:20:54 -0400

    started story 2

--- story2.py
+++ story2.py
@@ -2,8 +2,13 @@ from getInput import *
 
 def playMadlibs():
     friend1 = getWord("Enter a Name: ",)
+    umAnimals = getNumber("Enter a number: ", 1, 100)
+    #day = getWord("Enter a day: ",)
+    
+    
     
     output = ""
-    output += "One day I was " + adverb + " with my friend, " + friend1
+    output += friend1 + " is " + umAnimals + " years old"
+    #output +="Every week on " + day + ", " + 
     
     return output
