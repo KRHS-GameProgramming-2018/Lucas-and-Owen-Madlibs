31d8f09a3fd429748d49b5893d343e48bada65d3
Lucas Carey <lucas.carey@kearsarge.org>
2018-10-03 08:11:40 -0400

    starting story 2

--- story2.py
+++ story2.py
@@ -4,9 +4,6 @@ def playMadlibs():
     friend1 = getWord("Enter a Name: ",)
     
     output = ""
-    
-    
-    
-    
+    output += "One day I was " + adverb + " with my friend, " + friend1
     
     return output
