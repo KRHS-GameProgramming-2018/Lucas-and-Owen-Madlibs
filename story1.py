11ad3ca881de3feac678bf8f6e3b83aad08ec97a
Lucas Carey <lucas.carey@kearsarge.org>
2018-10-04 07:55:24 -0400

    Finished story 1

--- story1.py
+++ story1.py
@@ -6,7 +6,7 @@ def playMadlibs():
     balls1 = getWord("Enter a plural ball name: ")
     object1 = getWord("Enter an object: ")
     verb = getWord("Enter an verb: ")
-    adverb = getWord("Enter an adverb: ")
+    adverb = getWord("Enter an adverb ending in ly: ")
     day = getWord("Enter a day: ")
     food = getWord("Eter a food: ")
     wordEndsInED = getEd ("Enter a word that ends in ed:")
@@ -16,11 +16,11 @@ def playMadlibs():
     output += ". Suddenly " + friend1
     output += " said that they saw " + numAnimals + " " + balls1 + "."
     output +=   friend1 +" took out his " + object1 + ","
-    output += " and began to run after the ball. "
+    output += " and began to run after the balls. "
     output +=   friend1 + " got all "  + numAnimals + " " + balls1 + ". "
-    output += " The" + friend1 + " ran back to friend and gave them the " + balls1 + ". "
+    output += " Then " + friend1 + " ran back to friend and gave them the " + balls1 + ". "
     output += " The friend then said, thank god it's " + day + ". "
     output += " The friend said to " + friend1 + " lets go get some " + food + "."
-    output += " They walked to the store for the " + food + " and " + wordEndsInED + " trying the food"
+    output += " They walked to the store for the " + food + " and " + wordEndsInED + " trying the food."
     
     return output
