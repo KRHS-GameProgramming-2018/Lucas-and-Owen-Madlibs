213387e8c953b7c2c4aa09b7230b6302fb2cfcc1
Lucas Carey <lucas.carey@kearsarge.org>
2018-10-09 08:24:52 -0400

    updated story 2

--- story2.py
+++ story2.py
@@ -2,16 +2,22 @@ from getInput import *
 
 def playMadlibs():
     friend1 = getWord("Enter a Name: ")
-    umAnimals = getNumber("Enter a number: ", 10, 100)
+    number = getNumber("Enter a number: ", 80, 100)
     day = getWord("Enter a day: ")
     restuarant = getWord("Enter a restuarant: ")
     month = getWord ("Enter a month: ")
     year = getWord ("Enter a year: ")
-
+    secondNum = getNumber("Enter a number: ", 1, 20)
+    place = getWord ("Enter a place")
+    loveHate = getWord ("Enter a Place")
     
     output = ""
-    output += friend1 + " is " + umAnimals + " years old."
+    output +=  friend1 + " is " + number + " years old."
     output +=" Every week on " + day + ", " + friend1 + " goes to " + restuarant + "."
-    output +=" Today is " + day + " " + month + " " + year + ". it is " + friend1 + "'s" + umAnimals + "th birthday"
+    output +=" Today is " + day + " the 6th, " + month + " " + year + ". It is " + friend1 + "'s " + number + "th birthday. "
+    output +=  friend1 + " has " + secondNum +  " grand children. " 
+    output +=" Every time " + friend1 + " wants to see his family he meets them in " + place + " ."
+    output +=" All of his grand children" + loveHate + " to see him"
+    
     
     return output
