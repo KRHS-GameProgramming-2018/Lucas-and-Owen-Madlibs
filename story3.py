894406a2f753cf5893523cc48c135fdd99b50a99
Owen Cottrill <owen.cottrill@kearsarge.org>
2018-10-09 08:23:25 -0400

    UPDATE

--- story3.py
+++ story3.py
@@ -1,6 +1,6 @@
 def playMadlibs ():   
     year= getWord ("enter a number:")
-    name1= getword ("Enter first and last name")
+    name1= getWord ("Enter first and last name")
     
 
 
@@ -12,6 +12,6 @@ def playMadlibs ():
 
 
 
-
+    
     output = +year " years ago, " +name1 " did the unthinkable. "
     output = 
