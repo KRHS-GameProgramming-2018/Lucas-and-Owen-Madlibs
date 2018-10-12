123af02d6ef56a7bfc2a28e8bf4169187292f0ba
Lucas Carey <lucas.carey@kearsarge.org>
2018-10-12 08:21:53 -0400

    Finished and completed story 2

--- story2.py
+++ story2.py
@@ -1,9 +1,10 @@
 from getInput import *
 
 def playMadlibs():
-    friend1 = getWord("Enter a Name: ")
+    realName = getWord ("Enter your real first and last name: ")
+    friend1 = getWord("Enter a male name: ")
     number = getNumber("Enter a number: ", 84, 90)
-    day = getWord("Enter a day: ")
+    day = getWord("Enter a day of the week: ")
     restuarant = getWord("Enter a restuarant: ")
     month = getWord ("Enter a month: ")
     year = getWord ("Enter a year: ")
@@ -12,6 +13,9 @@ def playMadlibs():
     loveHate = getWord ("Enter the word love or hate: ")
     nameEndingInY = getY ("Enter a women's name ending in Y: ")
     food = getWord ("Enter a food: ")
+    job = getWord ("Enter a job: ")
+    bookGenre = getWord ("Enter a book Genre: ")
+    bowlingBingo = getWord ("Enter the word bowling or bingo: ")
     
     output = ""
     output +=  friend1 + " is " + number + " years old."
@@ -21,5 +25,8 @@ def playMadlibs():
     output +=" Every time " + friend1 + " wants to see his family he meets them in " + place + "."
     output +=" All of his grand children " + loveHate + " seeing him. "
     output +=  friend1 + " also has a wife named " + nameEndingInY + ". " + nameEndingInY + " is " + number + " too. "
+    output +=  friend1 + " and " + nameEndingInY + " are both retired, they used to work as " + job + "'s ."
+    output +=" Now they spend their time reading " + bookGenre + " books and enjoy going " + bowlingBingo + " from time to time."
+    output +=" The story is now over, thanks for playing " + realName + ". "
     
     return output
